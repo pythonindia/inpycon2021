5cc36362aa3966dffedb12c911d1502623f2a3db
Abhishek Mishra <geekabhi@outlook.com>
2021-03-03 15:50:22 +0530

    Update conference date
    Signed-off-by: Abhishek Mishra <geekabhi@outlook.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,7 +54,7 @@ $(document).ready(function() {
 
 	/* ======= Countdown ========= */
 	// set the date we're counting down to
-    var target_date = new Date("Sep 16, 2021").getTime();
+    var target_date = new Date("Sep 17, 2021").getTime();
 
     // variables for time units
     var days, hours, minutes, seconds;
